4091a1640ee11b011e0e01c61aa7aee0e06c86bd
rafael <rafaelarturonaranjo@gmail.com>
2016-09-06 15:28:10 -0400

    web

---
+++ web/js/payio.js
@@ -0,0 +1,33 @@
+(function($){
+  $(function(){
+
+    $(document).ready(function(){
+     $('.slider').slider({full_width: true});
+    })
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    })
+  })
+ $(document).ready(function(){
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 0
+    });
+  })
+  ;
+})(jQuery)
+
+new WOW().init();
+
+/*smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+    selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    updateURL: true, // Boolean. If true, update the URL hash on scroll
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+});*/
+
+
+
+
